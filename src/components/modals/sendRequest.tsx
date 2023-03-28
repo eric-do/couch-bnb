c19691dce8dd2454958e98d0e60d9765ac357f72
Eric Do <ericdo.617@gmail.com>
2023-03-27 23:14:08 -0600

    Adds date inputs

--- src/components/modals/sendRequest.tsx
+++ src/components/modals/sendRequest.tsx
@@ -7,9 +7,20 @@ interface Props {
 export default function SendRequestModal({ onClose }: Props) {
   return (
     <Modal onClose={onClose}>
-      <div className="w-96 flex justify-center items-center flex-col">
+      <div className="w-96 flex justify-center flex-col space-y-5">
         <h1 className="text-2xl font-bold">Send Request</h1>
-        <textarea ></textarea>
+        <div className="flex flex-row justify-between items-start">
+          <div className="flex flex-row justify-start">
+            <div className="pr-1">From:</div>
+            <input type="date"  className="border border-black pl-2"/>
+          </div>
+          <div className="flex flex-row justify-start">
+            <div className="pr-1">To:</div>
+            <input type="date"  className="border border-black pl-2"/>
+          </div>
+
+        </div>
+        <textarea className="border border-black p-2 resize-none w-full"></textarea>
         <button className="btn btn-primary">Send Request</button>
       </div>
     </Modal>
